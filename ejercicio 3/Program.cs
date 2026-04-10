e997fbffa36023f35c43d325d7001ad10327f540
pedro mendez <prmendez222@gmail.com>
2026-04-10 08:36:36 -0600

    ejercicio 4

--- ejercicio 3/Program.cs
+++ ejercicio 3/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Producto p = new Producto();
+
+p.Precio = 10;
+Console.WriteLine(p.Precio);
+class Producto
+{
+	private double precio;
+
+	public  double Precio
+	{
+		get 
+		{
+			if (precio > 0) return precio;
+			else return 0;
+		}
+		set { precio = value; }
+	}
+
+}
---
+++ ejercicio 4/Program.cs
@@ -0,0 +1,27 @@
+﻿Empleado e = new Empleado();
+
+e.Salario = 3000;
+
+if (e.Salario == 0)
+{
+    Console.WriteLine("salario no valido");
+}
+else
+{
+    Console.WriteLine(e.Salario);
+}
+class Empleado
+{
+    private double salario;
+
+    public double Salario
+    {
+        get
+        {
+            if (salario >= 3500) return salario;
+            else return 0;
+        }
+        set { salario = value; }
+    }
+
+}
