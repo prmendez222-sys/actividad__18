fd2764ec1d3c2a0cb44c63d97cdb0f0f5433261d
pedro mendez <prmendez222@gmail.com>
2026-04-10 08:24:38 -0600

    ejercicio 1

---
+++ ejercicio 1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+CuentaBancaria c = new CuentaBancaria();
+
+c.Saldo = 0;
+
+if (c.Saldo > 0) Console.WriteLine(c.Saldo);
+else Console.WriteLine("Saldo no disponible");
+class CuentaBancaria
+{
+	private double saldo;
+
+	public double Saldo
+	{
+		get 
+		{
+			if (saldo > 0) return saldo;
+			else return 0;
+		}
+		set { if (value > 0) saldo = value; }
+	}
+}
