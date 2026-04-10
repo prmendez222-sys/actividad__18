21ac0795f9beb07803bc75531a9e5d3cf9ca7203
pedro mendez <prmendez222@gmail.com>
2026-04-10 08:29:50 -0600

    ejercicio 2

---
+++ ejercicio 2/Program.cs
@@ -0,0 +1,19 @@
+﻿Estudiante e = new Estudiante();
+e.Edad = 30;
+
+Console.WriteLine(e.Edad);
+class Estudiante
+{
+	private int edad;
+
+	public int Edad
+	{
+		get 
+		{ 
+		    if(edad>=0 && edad<=100) return edad;
+			else return 0;
+		}
+		set { edad = value; }
+	}
+
+}
