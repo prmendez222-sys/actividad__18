1144328751ffe0900d74cdcbd7840a14913d2803
pedro mendez <prmendez222@gmail.com>
2026-04-10 08:43:26 -0600

    ejercicio 5

---
+++ ejercicio 5/Program.cs
@@ -0,0 +1,19 @@
+﻿Usuario u = new Usuario();
+
+u.Contrasena = "abc";
+Console.WriteLine(u.Contrasena);
+class Usuario
+{
+    private string contrasena;
+
+	public String Contrasena
+	{
+		get 
+		{
+			if (contrasena.Length >= 8) return "*********";
+			else return "Contrasena insegura";
+		}
+		set { contrasena = value; }
+	}
+
+}
